4afd7110a27989e3e1dfa310a2a945e8b4e460ad
Jsaulbautista <150806077+Jsaulbautista@users.noreply.github.com>
2024-01-29 22:49:04 -0500

    p1

--- src/Main.java
+++ src/Main.java
@@ -11,33 +11,5 @@ public class Main {
         loginn.setSize(400,200 );
 
 
-        String DBurl = "jdbc:mysql://localhost:3306/estudiantes";
-        String DBusername = "root";
-        String DBpassword = "";
-        java.sql.Connection connection = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(DBurl, DBusername, DBpassword);
-            Statement statement;
-            statement = connection.createStatement();
-            java.sql.ResultSet resultSet;
-            resultSet = statement.executeQuery("SELECT * FROM calificaciones");
-            int id;
-            String nombre, cedula;
-            float calificacion_1, calificacion_2;
-            while (resultSet.next()){
-                id = resultSet.getInt("id");
-                nombre = resultSet.getString("username");
-                cedula = resultSet.getString("cedula");
-                calificacion_1 = resultSet.getFloat("calificacion1");
-                calificacion_2 = resultSet.getFloat("calificacion2");
-                System.out.println("ID: "+id+" Nombre: "+nombre+" Cédula: "+cedula+" Calificación 1: "+calificacion_1+" Califcación 2: "+calificacion_2);
-            }
-
-        }
-        catch(Exception e){
-            System.out.println("ex");
-        }
-
     }
 }
--- src/login.java
+++ src/login.java
@@ -1,24 +1,65 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Arrays;
 
 public class login {
     public JPanel login;
     private JFormattedTextField usuarioText;
-    private JPasswordField passText;
+    public JPasswordField passText;
     private JButton button1;
 
     public login() {
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame nuevaPag = new JFrame("nueva Pagina");
-                nuevaPag.setContentPane(new nuevaPagina().nuevo);
-                nuevaPag.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-                nuevaPag.setVisible(true);
-                nuevaPag.setSize(500,200);
+                try {
+                    String pas = new String(passText.getPassword());
+                    String user, contrasena;
+                    if (!usuarioText.getText().isEmpty() && !pas.isEmpty()) {
 
-                Main.loginn.dispose();
+
+                        String DBurl = "jdbc:mysql://localhost:3307/usuario";
+                        String DBusername = "root";
+                        String DBpassword = "";
+                        java.sql.Connection connection = null;
+                        try {
+                            Class.forName("com.mysql.cj.jdbc.Driver");
+                            connection = DriverManager.getConnection(DBurl, DBusername, DBpassword);
+                            Statement statement;
+                            statement = connection.createStatement();
+                            java.sql.ResultSet resultSet;
+                            resultSet = statement.executeQuery("SELECT * FROM usuarios");
+
+                            while (resultSet.next()){
+                                user = resultSet.getString("usuarios");
+                                contrasena = resultSet.getString("contrasena");
+                                System.out.println("usuario: " + user +  " contrasena: " + contrasena);
+
+                                contrasena = resultSet.getString("contrasena");
+                                if (user.equals(usuarioText.getText()) || contrasena.equals(pas)) {
+
+                                    JFrame nuevaPag = new JFrame("nueva Pagina");
+                                    nuevaPag.setContentPane(new nuevaPagina().nuevo);
+                                    nuevaPag.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                                    nuevaPag.setVisible(true);
+                                    nuevaPag.setSize(500, 200);
+                                    Main.loginn.dispose();
+
+
+                                }
+                            }
+                        } catch (Exception e1) {
+                            System.out.println("No vale");
+                        }
+
+                    }
+                }catch (Exception e1){
+                    System.out.println("No valio");
+
+                }
             }
         });
     }
